213286a9140afb251615d937500e0bcb57352bd2
MxMstrmn <leon.het@googlemail.com>
2017-07-27 02:48:39 +0200

    Programme: Expanded Exzitonspektrum for the magnetoexciton spectrum and eigenvalues

--- Programme/MatLab/MX_Spektrum.m
+++ Programme/MatLab/MX_Spektrum.m
@@ -1,3 +1,5 @@
+const       = constants ; 
+h           = const.hbar ; 
 mu          = (0.41*const.e*0.46)/(0.41+0.46) ;             % m*e = 0.46me & m*h = 0.41mh
 EB          = h^2/(2*mu*const.a0^2) ;
 B           = 3*1e3*const.e ;
--- Programme/MatLab/Magnetoexziton.m
+++ Programme/MatLab/Magnetoexziton.m
@@ -1,27 +1,52 @@
-const       = constants ; 
-h           = const.hbar ; 
-n           = 300 ;
+function [out1, out2] = Magnetoexziton(Input,Method,Potential) 
 
-lambda      = 2;
-coufac      = (2*lambda/pi)^0.5 ;                              % Coulomb Vorfaktor
+[n, lambda, phi]    = deal(Input.n, Input.lambda, Input.phi) ;
+dim                 = n+1 ; 
 
 
-
-[n2,n1]     = meshgrid(0:n) ; 
-
-VC_ij       = @(n)   -coufac *exp(gamma_prefactor(0:n)) .*F32(0:n) ;
-Hmx_ii      = @(n1,n2)   lambda*(2*n1+1)  .*(n1==n2);
-Inh_ii      = @(n1,n2,phi) (-phi -1i*0.2)  *(n1==n2);       % phi = hw-Eg / EB
-
-b           = ones(n+1,1) ;
-X           = [] ;
-phi         = linspace(-10,45,1000);
-
-H           = Hmx_ii(n1,n2) + VC_ij(n) ;
-for i=1:length(phi)
-    A       = H + Inh_ii(n1,n2,phi(i)) ;
-    x_n     = linsolve(A,b) ;
-    x       = sum(x_n)*lambda/pi;
-    X       = [X x] ;
+switch Potential
+    case 'Coulomb'
+        const       =     (2*lambda/pi)^0.5 ;                            
+        VC_ij       = @(n) - const *exp(gamma_prefactor(0:n)) .*F32(0:n) ;
+    case 'Keldysh'
 end
+
+Hmx_ii      = @(n)      diag     (  lambda*(2*(0:n)+1) ) ;
+Inh_ii      = @(n,phi)  eye(dim)*( -phi    -1i*0.2     ) ;      
+
+H           = Hmx_ii(n) + VC_ij(n) ;
+
+
+switch Method
+    case 'Spektrum'
+        b           = ones (     dim    ,1) ;
+        X           = zeros(length(phi) ,1) ;
+        
+        for i=1:length(phi)
+            A       = H + Inh_ii(n,phi(i)) ;
+            x_n     = linsolve(A,b) ;
+            X(i)    = sum(x_n)*lambda/pi ;
+        end
+        out1        = X ;
+        out2        = phi ;
+    case 'Eigenwerte'
+         disp('Eigenwerte')
+        % Bestimmung der Eigenwerte (eig_val) samt Normierung der Wellenfunktionen
+        % (states). Beides beginnend mit dem Grundzustand (sort).
+        [states, EW]   = eig(H,'vector');
+        [EW, idx]      = sort(EW);
+        states         = states(:,idx);
+        EW             = EW(EW<50) ;
+        states         = states(:,EW<50) ;
+        [nn,g_nn]      = integrate([0 dim],dim,4) ; 
+        norm           = sqrt(2*pi*(states.^2)'*(nn.*g_nn));
+        for i=1:length(EW); states(:,i) = states(:,i)*1/norm(i)*sign(states(1,i)); end
+         
+        % Anzeigen der Grundzustandsenergie
+        disp(EW(1))
+        out1            = EW ;
+        out2            = states ;
+end
+
+
 
